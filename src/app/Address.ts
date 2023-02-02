13860e207d585b6483d6dbfc2ad75fae5407a8fb
vgeordy <vgeordy21@gmail.com>
2023-02-02 13:09:48 -0500

    Add Address Service to handle CRUD operations

---
+++ src/app/Address.ts
@@ -0,0 +1,7 @@
+export interface Address {
+    id?: number;
+    name: string;
+    address: string;
+    email: string;
+
+}
---
+++ src/app/services/address.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable} from 'rxjs';
+import { Address } from '../Address';
+import { ADDRESS } from 'src/app/mock-address';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AddressService {
+  private apiUrl: string = "http://localhost:3000/addresses"
+
+  constructor(private http: HttpClient) { }
+
+  getAddresses():Observable<Address[]> {
+    return this.http.get<Address[]>(this.apiUrl);
+  }
+
+  deleteAddress(address: Address): Observable<Address> {
+    const url = `${this.apiUrl}/${address.id}`;
+    return this.http.delete<Address>(url,httpOptions)
+  }
+
+  editAddress(address: Address): Observable<Address> {
+    const url = `${this.apiUrl}/${address.id}`;
+    return this.http.put<Address>(url,address, httpOptions);
+  }
+
+}
