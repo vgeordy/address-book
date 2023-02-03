19f42dc18bbe5906f1993600ec9f79d6374dffff
vgeordy <vgeordy21@gmail.com>
2023-02-03 13:56:20 -0500

    Add ui service to header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { AddressService } from 'src/app/services/address.service';
+import { Address } from 'src/app/Address';
+import { MatDialog } from '@angular/material/dialog';
+import { AddressDialogComponent } from '../address-dialog/address-dialog.component';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+import { __values } from 'tslib';
+
 
 @Component({
   selector: 'app-header',
@@ -8,4 +16,23 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
 
   title: string = "ADDRESS BOOK";
-}
+  address!: Address;
+  subscription!: Subscription;
+
+
+  constructor(private addressService: AddressService, private uiService: UiService) {
+    this.subscription = this.uiService.onAddClick().subscribe((value) => (this.address = value));
+  }
+
+
+  onAddAddress() {
+    console.log(1234);
+    this.uiService.addUIAddress()
+    }
+
+
+
+  }
+
+
+
