81c9a0c15672fc716968fb1e5dc8879a7f52a8f2
vgeordy <vgeordy21@gmail.com>
2023-02-05 12:43:19 -0500

    Refactor Header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,34 +5,24 @@ import { MatDialog } from '@angular/material/dialog';
 import { AddressDialogComponent } from '../address-dialog/address-dialog.component';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
-import { __values } from 'tslib';
-
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent {
 
   title: string = "ADDRESS BOOK";
-  address!: Address;
-  subscription!: Subscription;
 
-
-  constructor(private addressService: AddressService, private uiService: UiService) {
-    this.subscription = this.uiService.onAddClick().subscribe((value) => (this.address = value));
-  }
-
+  constructor(private uiService: UiService) {}
 
   onAddAddress() {
-    console.log(1234);
-    this.uiService.addUIAddress()
-    }
-
-
-
+    this.uiService.addUIAddress();
+  
   }
+}
 
 
 
