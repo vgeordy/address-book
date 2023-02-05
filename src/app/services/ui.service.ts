bdef08ed1944d9954ed3764990e38f7b9379f71c
vgeordy <vgeordy21@gmail.com>
2023-02-05 12:40:58 -0500

    Change addUIAddress and Add editUIAddress function

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -1,37 +1,34 @@
 import { Injectable } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import {Observable, Subject} from 'rxjs';
 import { Address } from '../Address';
 import { AddressDialogComponent } from '../components/address-dialog/address-dialog.component';
+import { AddressService } from './address.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UiService {
-  private newAddress!: Address;
-  private subject = new Subject<any>();
 
-
-
-  constructor(private dialog: MatDialog) { }
-
+  constructor(private dialog: MatDialog, private addressService: AddressService) { }
 
   addUIAddress() {
-    const dialogRef = this.dialog.open(AddressDialogComponent, {
-      width: '500px',
-      height: '500px',
-      data: {}
-    });
-  
-    
-    dialogRef.afterClosed().subscribe(result => (this.newAddress = result));
-    console.log(this.newAddress);
-    this.subject.next(this.newAddress);
+    this.addressService.initializeFormGroup();
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "60%";
+    this.dialog.open(AddressDialogComponent, dialogConfig);
   }
 
-  onAddClick(): Observable<any> {
-    return this.subject.asObservable();
+  editUIAddress(row: any) {
+    this.addressService.populateAddressForm(row);
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "60%";
+    this.dialog.open(AddressDialogComponent, dialogConfig);
+
   }
-
-
 }
 
