6f46abfe10d60c671f733b6cf773a756f50976c8
vgeordy <vgeordy21@gmail.com>
2023-02-06 23:14:24 -0500

    Change Address Dialog html and css

--- src/app/components/address-dialog/address-dialog.component.ts
+++ src/app/components/address-dialog/address-dialog.component.ts
@@ -1,31 +1,35 @@
-import { Component, EventEmitter, Inject, OnInit, Output} from '@angular/core';
-import { Address } from 'src/app/Address';
-import { MatDialog, MatDialogRef} from '@angular/material/dialog';
+import { Component,OnInit} from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 import { AddressService } from 'src/app/services/address.service';
 import { ApiService } from 'src/app/services/api.service';
-
 
 @Component({
   selector: 'app-address-dialog',
   templateUrl: './address-dialog.component.html',
-  styleUrls: ['./address-dialog.component.css']
+  styleUrls: ['./address-dialog.component.css'],
 })
 
-
 export class AddressDialogComponent implements OnInit {
+  constructor(
+    public apiService: ApiService,
+    public addressService: AddressService,
+    public dialogRef: MatDialogRef<AddressDialogComponent>
+  ) {}
 
-  constructor(public apiService: ApiService, public addressService: AddressService, public dialogRef: MatDialogRef<AddressDialogComponent>) {}
-  
   ngOnInit() {
+    console.log(123);
     this.apiService.getAddresses();
   }
-  
+
   onSubmit(value: any): void {
     if (this.addressService.addressForm.valid) {
-        if (!(this.addressService.addressForm.get('id')!.value) ) {
+        if (!(this.addressService.addressForm.get('id')!.value)) {
+          console.log("from onSubmit function");
           this.addressService.createAddress(value);
+          this.apiService.getAddresses().subscribe((addresses)=> (this.addressService.addresses = addresses));
       }else {
-        this.addressService.updateAddress(value);
+        this.addressService.updateAddress(value); 
+        window.location.reload();
         this.onClose();
       }
     }
@@ -37,7 +41,3 @@ export class AddressDialogComponent implements OnInit {
     this.dialogRef.close();
   }
 }
-
-
-
-
