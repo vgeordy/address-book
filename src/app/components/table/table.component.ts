c97f585ec1b14a075e09d35c558dda4ea622826c
vgeordy <vgeordy21@gmail.com>
2023-02-06 23:19:11 -0500

    Remove unused imports in Table component

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,30 +1,33 @@
-import { Component, EventEmitter, OnInit, Output, Input, OnChanges } from '@angular/core';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { Subscription } from 'rxjs';
-import { Address } from 'src/app/Address';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
 import { AddressService } from 'src/app/services/address.service';
 import { ApiService } from 'src/app/services/api.service';
 import { UiService } from 'src/app/services/ui.service';
-import { AddressDialogComponent } from '../address-dialog/address-dialog.component';
+
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
+export class TableComponent implements OnInit {
+  displayedColumns = ['id', 'name', 'address', 'email', 'phone-number','actions'];
+  
 
-export class TableComponent implements OnInit{
-  displayedColumns = ['id', 'name', 'address','email','actions'];
-  subscription!: Subscription;
-
-  constructor(public apiService: ApiService, public dialog: MatDialog, public uiService: UiService, public addressService: AddressService) {
-   // this.subscription = this.addressService.onChange().subscribe();
-  }
+  constructor(
+    public apiService: ApiService,
+    public uiService: UiService,
+    public addressService: AddressService
+    
+  ) { }
 
   ngOnInit(): void {
-    this.apiService.getAddresses().subscribe((addresses)=> (this.addressService.addresses = addresses));
-  }
+    this.apiService
+      .getAddresses()
+      .subscribe((addresses) => (this.addressService.addresses = addresses));
 
-  
-  
+      
+  }
 }
