c2a7d5cba310ec1a8245d704003d7bb39c1710bf
vgeordy <vgeordy21@gmail.com>
2023-02-06 23:15:38 -0500

    Add refresh button to header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { AddressService } from 'src/app/services/address.service';
-import { Address } from 'src/app/Address';
-import { MatDialog } from '@angular/material/dialog';
-import { AddressDialogComponent } from '../address-dialog/address-dialog.component';
+import { Component} from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
-import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
@@ -22,6 +18,12 @@ export class HeaderComponent {
     this.uiService.addUIAddress();
   
   }
+  
+  onClick() {
+    window.location.reload();
+  }
+
+
 }
 
 
