c7fb2660dfaacc1c4d68f085282ea5fe7feeb4ac
vgeordy <vgeordy21@gmail.com>
2023-02-06 23:18:19 -0500

    Remove unused imports in UI service

--- src/app/services/ui.service.ts
+++ src/app/services/ui.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import {Observable, Subject} from 'rxjs';
-import { Address } from '../Address';
 import { AddressDialogComponent } from '../components/address-dialog/address-dialog.component';
 import { AddressService } from './address.service';
 
